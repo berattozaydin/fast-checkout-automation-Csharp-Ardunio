1a4a49a031686f75be082d34da76c0779cab4dd7
Berat <berattozaydin@gmail.com>
2023-01-04 10:01:06 +0300

    asdads

--- qwe/Controllers/WeatherForecastController.cs
+++ qwe/Controllers/WeatherForecastController.cs
@@ -11,11 +11,11 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _loggertr;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> loggerr)
     {
-        _logger = logger;
+        _loggertr = loggerr;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
